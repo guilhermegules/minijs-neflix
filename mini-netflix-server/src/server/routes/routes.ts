1fd401e306d343fe7033b377b57dc2f0220fe034
Guilherme Gules Moreira <guilherme.moreira@ilegra.com>
2020-03-31 15:18:03 -0300

    (refactor): melhorando docker

--- mini-netflix-server/src/server/routes/routes.ts
+++ mini-netflix-server/src/server/routes/routes.ts
@@ -1,22 +1,22 @@
 import { Router } from 'express'
-// import axios from 'axios'
+import axios from 'axios'
 import dotenv from 'dotenv'
 import { users } from '../user/users'
 dotenv.config()
 
-// const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=netflix%20trailer&type=video'
+const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=netflix%20trailer&type=video'
 
 const routes = Router()
 
-// routes.get('/videos', (req, res) => {
-//   axios.get(`${url}&key=${process.env.GOOGLE_API_KEY}`)
-//     .then(result => {
-//       res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
-//       res.send(result.data.items)
-//     }).catch(err => {
-//       res.send(err)
-//     })
-// })
+routes.get('/videos', (req, res) => {
+  axios.get(`${url}&key=${process.env.GOOGLE_API_KEY}`)
+    .then(result => {
+      res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+      res.send(result.data.items)
+    }).catch(err => {
+      res.send(err)
+    })
+})
 
 routes.get('/users', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
@@ -24,7 +24,7 @@ routes.get('/users', (req, res) => {
 })
 
 routes.get('/', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200')
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8081')
   res.send('access /videos from videos info <br> acess /login from users info')
 })
 
